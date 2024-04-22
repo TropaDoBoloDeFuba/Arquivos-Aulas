6cf90a4169b2fa8bfef75ded0d867fe6c6958c29
Rodolfo Gama <rodolfo.gama2006@gmail.com>
2024-04-22 18:47:51 -0300

    Sync

---
+++ POO/Curso.java
@@ -0,0 +1,9 @@
+public class Curso {
+    private String nome;
+    public Curso(String nome) {
+        this.nome = nome; 
+    }
+    public String getNome() {
+        return this.nome;
+    }
+}
