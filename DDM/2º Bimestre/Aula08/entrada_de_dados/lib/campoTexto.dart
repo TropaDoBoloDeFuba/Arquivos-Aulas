a6adb596994dbb0588726c3be098b13f0d8efeb4
Tropa do Bolo de Fubá <tropadobolodefuba@gmail.com>
2024-06-05 12:08:15 -0300

    DDM B2A08 Part2

--- DDM/2º Bimestre/Aula08/entrada_de_dados/lib/campoTexto.dart
+++ DDM/2º Bimestre/Aula08/entrada_de_dados/lib/campoTexto.dart
@@ -1,36 +1,95 @@
-import "package:flutter/material.dart";
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CampoTexto extends StatefulWidget {
-  const CampoTexto({super.key});
+  const CampoTexto({Key? key}) : super(key: key);
 
   @override
   State<CampoTexto> createState() => _CampoTextoState();
 }
 
 class _CampoTextoState extends State<CampoTexto> {
+  bool _obscureText = true;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Entrada de dados"),
+        titleTextStyle: const TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+        ),
       ),
-      body: Column(children: const <Widget>[
-        Padding(padding: EdgeInsets.all(32),
-        child: TextField(
-          keyboardType: TextInputType.text,
-          decoration: InputDecoration(
-            labelText: "Digite o nome"
+      body: Column(
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.all(32),
+            child: TextField(
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: "Digite o nome",
+              ),
+              enabled: true,
+              maxLength: 50,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black54,
+              ),
+              obscureText: false,
+            ),
           ),
-          enabled: true,
-          maxLength: 50,
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.black26
+          const Padding(
+            padding: EdgeInsets.all(32),
+            child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: "Digite seu email",
+              ),
+              enabled: true,
+              maxLength: 50,
+              style: TextStyle(color: Colors.black87),
+            ),
           ),
-          obscureText: true,
-        ),)
-      ]),
+          Padding(
+            padding: const EdgeInsets.all(32),
+            child: TextField(
+              keyboardType: TextInputType.visiblePassword,
+              decoration: InputDecoration(
+                labelText: "Digite sua senha",
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _obscureText ? Icons.visibility : Icons.visibility_off,
+                    color: Colors.grey,
+                  ),
+                  onPressed: _togglePasswordVisibility,
+                ),
+              ),
+              enabled: true,
+              obscureText: _obscureText,
+              maxLength: 20,
+            ),
+          ),
+          ElevatedButton(
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              backgroundColor: MaterialStateProperty.all(Colors.green[700]),
+              elevation: MaterialStateProperty.all(3),
+            ),
+            onPressed: () {},
+            child: const Text('Enviar'),
+          ),
+        ],
+      ),
     );
   }
 }
